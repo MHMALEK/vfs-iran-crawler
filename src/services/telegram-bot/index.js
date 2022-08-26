eb7e1442929b0c445d1ed5d2c4b17b80f53f38d7
Mohammad Hossein Malek <mhos.malek@gmail.com>
2022-08-27 01:31:00 +0200

    ready to deploy

--- src/services/telegram-bot/index.js
+++ src/services/telegram-bot/index.js
@@ -10,11 +10,51 @@ const TelegramBot = createTelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
 const initTelegramBotListeners = () => {
   TelegramBot.on("message", onMessageRecived);
+  TelegramBot.on("callback_query", onCallbackQuery);
+  TelegramBot.onText(/\/start/, showStartMenu);
+};
+
+const createStartButtons = () => {
+  const options = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{ text: "Notify me!", callback_data: "NOTIFY" }],
+        [{ text: "About Bot", callback_data: "ABOUT" }],
+      ],
+    }),
+  };
+  return options;
+};
+
+const showStartMenu = (msg) => {
+  const chatId = msg.chat.id;
+  TelegramBot.sendMessage(chatId, "Please choose", createStartButtons());
 };
 
 const onMessageRecived = (msg) => {
   const chatId = msg.chat.id;
   saveChatIDToDB(chatId);
+
+  showStartMenu();
+};
+
+const onCallbackQuery = (callbackQuery) => {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+  };
+  let text;
+
+  if (action === "NOTIFY") {
+    text = "You will be notified when there is a new slot available";
+  }
+  if (action === "ABOUT") {
+    text = "You can read more about this bot on the website";
+  }
+
+  TelegramBot.editMessageText(text, opts);
 };
 const saveChatIDToDB = async (chatId) => {
   try {
