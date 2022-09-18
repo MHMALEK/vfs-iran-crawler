43554c9c389f416120830fcf830be08090ed0c23
Mohammad Hossein Malek <mhos.malek@gmail.com>
2022-09-18 15:47:12 +0200

    send message to telegram users

--- src/controllers/checkAppointment.controller.js
+++ src/controllers/checkAppointment.controller.js
@@ -1,8 +1,10 @@
 import AppointmentCheckerService from "../services/appointment-checker";
+import { sendMessageToAllUsers } from "../services/telegram-bot";
 
 const checkAppointmentsController = async (req, res, next) => {
   try {
     const result = await AppointmentCheckerService();
+    sendMessageToAllUsers(result)
     res.send(result);
   } catch (e) {
     next(e);
