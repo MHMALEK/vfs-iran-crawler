d4c7175a9ab44da853503f19f2155f0cc0b8f7f3
M.H.Malek <mhos.malek@gmail.com>
2023-05-30 22:24:24 +0200

    run cronjob everyhour

--- src/services/scheduldedFunctions.js
+++ src/services/scheduldedFunctions.js
@@ -3,7 +3,7 @@ const CronJob = require("node-cron");
 export const cronJobDefaultConfig = {
   // every day at 12 pm
   // expression: `0 12 * * *`,
-  expression: "*/30 * * * *",
+  expression: "0 * * * *",
   callBack: () => console.log("executed on a schedule!"),
 };
 const createScheduledJobs = ({
